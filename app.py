3dca571986d8def4147339c8b66de0736a113654
DamonToumbourou <dtoumbourou@gmail.com>
2016-03-09 08:19:42 +0000

    change app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "SEPT OG WEATHER SERVICE!"
+    return "SEPT WEATHER SERVICE!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
