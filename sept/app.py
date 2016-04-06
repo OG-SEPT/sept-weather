36f431535f1cdcb849e5408c0ad0ea6aed7bcbd5
Ben Diep <ben_diep@hotmail.com>
2016-04-06 15:28:28 +1000

    Delete app.py

--- sept/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "EPT WEATHER SERVICE!"
-
-if __name__ == "__main__":
-    app.run(host='localhost', port=5000)
