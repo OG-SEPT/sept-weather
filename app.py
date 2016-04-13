b52d4d5054740b669c930fcb2529902ec5b23282
Damon Toumbourou <damontoumbourou@Damons-MBP.gateway>
2016-04-13 18:26:33 +1000

    working on pulling data

--- app.py
+++ app.py
@@ -17,19 +17,55 @@ def home():
 
 @app.route('/victoria')
 def get_victoria():
-    return render_template('victoria.html')
+    url = "http://www.bom.gov.au/climate/dwo/IDCJDW0300.shtml"
+    stations = get_stations(url) 
+    
+    return render_template('victoria.html', stations=stations)
 
 
-@app.route('/api/get_weather', methods=['POST'])
-def get_weather():
-    station_url = request.get('station_url', '')
-    print station_url
-
+def get_stations(url):
     page = requests.get(url)
     soup = bs(page.text, 'html.parser')
-    answer = question
+    
+    content = soup.find_all('a')
+   
+    links = []
+    for con in content:
+        link = con.get('href')
+        link = re.search(r'climate/dwo/I(.*)', link)
+        if link:
+            found_link = link.group(0)
+            
+            item = { 
+                    'url': found_link,
+            }
 
-    return json.dumps({'answer': answer})
+            links.append(item)
+
+    all_links = []
+    for link in links:
+        link = "http://www.bom.gov.au/" + link['url']
+        page = requests.get(link)
+        soup = bs(page.text, 'html.parser')
+        
+        content = soup.find('div', {'class': 'content'})
+        content = content.find_all('a')
+        
+        for con in content:
+            link = con.get('href') 
+            link = re.search(r'(.*)latest.shtml', link)
+            if link:
+                location = con.get_text()
+                found_link = link.group(0)
+                
+                item = {
+                        'loction': location,
+                        'url': found_link,
+                }
+            
+                all_links.append(item)
+    
+    return all_links
 
 
 if __name__ == "__main__":
