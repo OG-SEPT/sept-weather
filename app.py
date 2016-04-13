e227eac6e4eae676530dee5e734664adf961e352
Damon Toumbourou <damontoumbourou@Damons-MBP.gateway>
2016-04-13 16:57:04 +1000

    working on layout

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup as bs
+import requests
+import re
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def home():
+    station_url = request.args.get('station_url')
+
     return render_template('index.html')
+
+
+@app.route('/victoria')
+def get_victoria():
+    return render_template('victoria.html')
+
+
+@app.route('/api/get_weather', methods=['POST'])
+def get_weather():
+    station_url = request.get('station_url', '')
+    print station_url
+
+    page = requests.get(url)
+    soup = bs(page.text, 'html.parser')
+    answer = question
+
+    return json.dumps({'answer': answer})
 
 
 if __name__ == "__main__":
