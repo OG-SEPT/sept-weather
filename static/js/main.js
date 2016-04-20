7346f0ec13a0f04d63876e08e07672bc16e89997
Damon Toumbourou <damontoumbourou@Damons-MacBook-Pro.local>
2016-04-20 15:36:33 +1000

    working on favorites

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+// loads the station names
 function loadFunction(name) {
      
     $.ajax({
@@ -6,36 +7,62 @@ function loadFunction(name) {
         data: name,
         dataType: 'json',
         success: function(result){
-            var items = document.getElementById('stations');
-            items.innerHTML = "";
+            var station_data = document.getElementById('station');
+            station_data.innerHTML = "";
+
+            var add_favorite = document.getElementById('add_favorite');
+            add_favorite.innerHTML = "";
 
             $.each(result, function(index, value){
                 var button = document.createElement('input');
-                var link = value.url;
                 button.setAttribute("type", "button");
                 button.setAttribute("value", value.station_name);
                 button.addEventListener('click', function(){
-                    getRequest(value.url);
+                    getStationData(value.url);
                 });
+                station_data.appendChild(button);
                 
-                items.appendChild(button);
                 
+                var fav_button = document.createElement('input');
+                fav_button.setAttribute("type", "button");
+                fav_button.setAttribute("value", "fav");
+                fav_button.addEventListener('click', function(){
+                    addFavorite(value.url);
+                });
+                station_data.appendChild(fav_button); 
             });
         }
     });
 }
 
-
-function getRequest(url){
+// adds a station to favorite 
+// display graph here
+function addFavorite(url){
+    
     $.ajax({
         type: 'GET',
-        url: "stations_info",
+        url: 'station_fav',
         data: url,
         dataType: 'json',
         success: function(result){
+            console.log("success!: " + result);
+        }
+    });
+}
+
+// loads the station weather data
+function getStationData(url){
+    $.ajax({
+        type: 'GET',
+        url: 'stations_info',
+        data: url,
+        dataType: 'json',
+        success: function(result){
+
+            // handles the wetaher station data
             var items = document.getElementById('station_data');
             items.innerHTML = "";
-                       
+            
             $.each(result, function(index, value){
                 var td = document.createElement('td');
                 td.textContent = value.date; 
@@ -127,6 +154,8 @@ function getRequest(url){
                          
                 var tr = document.createElement('tr');
                 items.appendChild(tr);
+
+
             });   
 
         }
