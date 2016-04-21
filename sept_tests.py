3c497f7838440e1ce71fafb47868a0fab1751592
Damon Toumbourou <damontoumbourou@Damons-MacBook-Pro.local>
2016-04-21 17:06:48 +1000

    damon is cool

---
+++ sept_tests.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+import tempfile
+import sept_weather
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def test_stations_info(self):
+        self.app = sept_weather.app.test_client()
+        result = self.app.get('/')
+        print result 
+
+        assert '<!DOCTYPE html>' in result.data
+    
+    def test_reset_database(self):
+        self.app = sept_weather.app.test_client()
+        result = self.app.get('/reset_database')
+        print result.data
+
+        assert 'Successful' in result.data
+
+if __name__ == '__main__':
+    unittest.main()
