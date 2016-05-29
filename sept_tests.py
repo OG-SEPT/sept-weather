d3383e249803833537cd23435cea27c2930f36d8
Damon Toumbourou <damontoumbourou@Damons-MBP.gateway>
2016-05-30 02:59:51 +1000

    tests added

--- sept_tests.py
+++ sept_tests.py
@@ -8,22 +8,15 @@ class FlaskrTestCase(unittest.TestCase):
     def test_stations_info(self):
         self.app = sept_weather.app.test_client()
         result = self.app.get('/')
-        print result 
 
         assert '<!DOCTYPE html>' in result.data
     
     def test_reset_database(self):
         self.app = sept_weather.app.test_client()
         result = self.app.get('/reset_database')
-        print result.data
 
         assert 'Successful' in result.data
     
-    def test_forecastapi(self):
-        self.app = sept_weather.app.test_client()
-        result = self.app.get('/forecaset_io')
-
-        assert 'ses' in result.data
 
 if __name__ == '__main__':
     unittest.main()
